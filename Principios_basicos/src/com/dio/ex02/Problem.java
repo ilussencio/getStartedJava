0af0d5d66ba2f0fad740c167f70bd7d818ad553a
Lucas Ilussencio da Silva <69581917+ilussencio@users.noreply.github.com>
2022-06-21 06:14:44 -0300

    Desafios DIO

---
+++ Principios_basicos/src/com/dio/ex02/Problem.java
@@ -0,0 +1,22 @@
+package com.dio.ex02;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Problem {
+    //complete os espaços em branco com uma solução possível
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        while (leitor.hasNext()) {
+            String alphabet = leitor.next();
+            String word = "";
+            int letters = leitor.nextInt();
+
+            for (int i = 0; i < letters; i++) {
+                word += alphabet.charAt(leitor.nextInt()-1);
+            }
+            System.out.println(word);
+        }
+    }
+
+}
